8e7330bb58ae5bc7203b818bc84cb888736c5563
Antonio B <39917231+Abourass@users.noreply.github.com>
2019-03-22 16:48:14 -0600

    Update and rename SecurityTokens.js to Tokens.js

---
+++ models/Tokens.js
@@ -0,0 +1,18 @@
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const TokenSchema = new Schema({
+    userId: {
+      type: String
+    },
+    userFullName: {
+      type: String
+    },
+    securityToken: {
+      type: String
+    }
+})
+
+// Create collection and add Schema
+mongoose.model('tokens', TokenSchema)
