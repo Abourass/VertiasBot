3227eff50473edbfbb8d10b6e6ad4e8739523942
Abourass <abourassa@assetval.com>
2019-03-22 16:40:59 -0600

    Init Commit

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ module.exports = async app => {
   const SecToken = require('./models/SecurityTokens')
   const keys = require('./config/keys')
   mongoose.connect(keys.mongoURI, { useNewUrlParser: true }).then(() => console.log('Atlas is shouldering our burden | Database Aloft!')).catch(err => console.log(err))
-  const appGitHub = await app.auth() 
-  const router = app.route('/api') 
-    // Use any middleware
+  const appGitHub = await app.auth()
+  const router = app.route('/api')
+  // Use any middleware
   router.use(require('express').static('public'))
   const owner = 'AssetVal'; const repo = 'AssetVal_Veritas'; let tagArray = []; let assArray = []
 
