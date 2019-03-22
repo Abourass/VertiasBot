40d4053ac202919270a7765524002b11bef25abd
Antonio B <39917231+Abourass@users.noreply.github.com>
2019-03-22 17:36:31 -0600

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb://buttler:3iIwoIklDHfICXxy@veritas-bot-shard-00-00-tliqo.mongodb.net:27017,veritas-bot-shard-00-01-tliqo.mongodb.net:27017,veritas-bot-shard-00-02-tliqo.mongodb.net:27017/test?ssl=true&replicaSet=Veritas-Bot-shard-0&authSource=admin&retryWrites=true'
+  mongoURI: 'mongodb+srv://buttler:3iIwoIklDHfICXxy@veritas-bot-tliqo.mongodb.net/vBot?retryWrites=true'
 }
